bd92d352e4494cefc6f4efc47e7c56cce98ed942
nguyen656 <63175927+nguyen656@users.noreply.github.com>
2021-10-11 20:34:39 +0700

    Create segment1.cpp

---
+++ 10Tin01/segment1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("segment1.inp", "r", stdin);
+    freopen("segment1.out", "w", stdout);
+    long long n;
+    cin >> n;
+    cout << n * (n - 1) / 2;
+    return 0;
+}
