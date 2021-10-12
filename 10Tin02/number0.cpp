20fd76bb07e4044890713aafd608042ea14778f1
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-10-12 15:03:22 +0700

    Create number0.cpp

---
+++ 10Tin02/number0.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("number0.inp", "r", stdin);
+    freopen("number0.out", "w", stdout);
+    long n, d = 0;
+    cin >> n;
+    while (n >= 5) {
+        n /= 5;
+        d += n;
+    }
+    cout << d;
+    return 0;
+}
