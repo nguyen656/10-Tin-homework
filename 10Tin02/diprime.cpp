ed7aeb4a7de2f6c8350346210ef5439866e00729
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-10-12 15:49:44 +0700

    Create diprime.cpp

---
+++ 10Tin02/diprime.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    freopen("diprime.inp", "r", stdin);
+    freopen("diprime.out", "w", stdout);
+    long n;
+    cin >> n;
+    map<int, int> m;
+    for(int i = 2; i <= n; i++) {
+        while(n % i == 0) {
+            m[i]++;
+            n /= i;
+        }
+    }
+    for (auto it : m) cout << it.first << " "; 
+    cout << "\n";
+    for (auto it : m) cout << it.second << " ";
+    return 0;
+}
+ 
