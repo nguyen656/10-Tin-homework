1ba6e692666cf1658683d00a8e6f9ee67635bb2e
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-10-13 14:44:14 +0700

    Update prime02.cpp

--- 10Tin03/prime02.cpp
+++ 10Tin03/prime02.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool prime(long n)
 {
     if (n == 2 || n == 3) return true;
-    if (n == 1 || n % 2 == 0 || n % 3 == 0) return false;
+    if (n <= 1 || n % 2 == 0 || n % 3 == 0) return false;
     long i = 5;
     while (i * i <= n) {
         if (n % i == 0 || n % (i + 2) == 0) return false;
