698c4ffc9f857fc59d0f68301d08b0f38d8d4208
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-10-15 21:41:18 +0700

    Update primec.cpp

--- 10Tin03/primec.cpp
+++ 10Tin03/primec.cpp
@@ -6,9 +6,7 @@ int main()
     freopen("primec.inp", "r", stdin);
     freopen("primec.out", "w", stdout);
     bool isPrime[1000001];
-    for(int i = 0; i <= 1000001;++i) {
-        isPrime[i] = true;
-    }
+    memset(isPrime, true, sizeof isPrime);
     isPrime[0] = false;
     isPrime[1] = false;
     for(int i = 2; i * i <= 1000001; ++i) {
