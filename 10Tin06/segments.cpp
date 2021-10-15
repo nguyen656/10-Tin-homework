0b2a022aef2c534a0fcfaa88fd25b0ac17ca129b
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-10-15 19:52:23 +0700

    Create segments.cpp

---
+++ 10Tin06/segments.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("segments.inp", "r", stdin);
+    freopen("segments.out", "w", stdout);
+    long n, a[1000000], d = 1, max = 0;
+    cin >> n >> a[0];
+    for (long i = 1; i < n; i++) {
+        cin >> a[i];
+        if (a[i] == a[i - 1]) {
+            ++d;
+            if (d > max) max = d;
+        }
+        else d = 1;
+    }
+    cout << max;
+    return 0;
+}
