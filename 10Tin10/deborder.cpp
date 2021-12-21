8d17ae9931c359933a76e6d2e4e23a26bd16ef7e
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-12-21 16:59:08 +0700

    Update deborder.cpp

--- 10Tin10/deborder.cpp
+++ 10Tin10/deborder.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 int main()
 {
-    freopen("DEBORDER.inp", "r", stdin);
-    freopen("DEBORDER.out", "w", stdout);
-    int d[21];
-	  d[0] = 0;
-	  d[1] = 1;
-	  d[2] = 3;
-	  for (int i = 3 ; i <= 20 ; i++) d[i] = d[i - 2] * 2 + d[i - 1];
-	  int n;
-	  while (cin >> n) cout << d[n] << "\n";
+    freopen("DEBORDER.inp","r",stdin);
+    freopen("DEBORDER.out","w",stdout);
+    int n, d[21];
+    d[0] = 0; d[1] = 1; d[2] = 3;
+    for (int i = 3; i <= 20; i++)
+	d[i] = d[i - 2] * 2 + d[i - 1];
+    while (cin >> n) cout << d[n] << "\n";
     return 0;
 }
