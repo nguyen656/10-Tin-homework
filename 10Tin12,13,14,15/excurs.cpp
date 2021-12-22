5cc9496ec4f1b70f6b3de6b9f3783f9c8965e41d
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-12-22 23:44:30 +0700

    Create excurs.cpp

---
+++ 10Tin12,13,14,15/excurs.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("EXCURS.inp", "r", stdin);
+    freopen("EXCURS.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long n, x, t = 0, lmax = 0;
+    cin >> n;
+    long l[n];
+    std::vector<long> a;
+    for (long i = 1; i <= n; i++) {
+        cin >> x;
+        if (x % 2 == 0 && x != t) {
+            a.push_back(x);
+            t = x;
+        }
+    }
+    n = a.size();
+    for (int i = 0; i < n; i++) {
+        l[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (a[i] > a[j]) l[i] = max(l[i], l[j] + 1);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        lmax = max(lmax, l[i]);
+    }
+    cout << lmax;
+    return 0;
+}
