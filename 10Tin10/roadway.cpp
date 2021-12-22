201ee7077854b9b471eb1088b2f0a60cceb71261
Tran Nguyen Le <63175927+nguyen656@users.noreply.github.com>
2021-12-22 23:09:45 +0700

    Create roadway.cpp

---
+++ 10Tin10/roadway.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string add(string a, string b)
+{
+    string res= "";
+    while (a.length() < b.length()) a = "0" + a;
+    while (b.length() < a.length()) b = "0" + b;
+    int carry = 0;
+    for (int i = a.length() - 1; i >= 0; i--)
+    {
+        int tmp = a[i] - 48 + b[i] - 48 + carry; 
+        carry = tmp / 10;
+        tmp = tmp % 10;
+        res = (char)(tmp + 48) + res;
+    }
+    if (carry > 0) res = "1" + res;
+    return res;
+}
+
+int main()
+{
+    freopen("ROADWAY.inp", "r", stdin);
+    freopen("ROADWAY.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+	  cin >> n >> m;
+	  string d[n + 1][m + 1];
+	  d[1][1] = "1";
+	  for (int i = 0 ; i <= n; i++) d[i][1] = "1";
+	  for (int i = 0 ; i <= m ; i++) d[1][i] = "1";
+	  for (int i = 2 ; i <= n ; i++)
+		    for (int j = 2 ; j <= m ; j++)
+			      d[i][j] = add(d[i - 1][j], d[i][j - 1]);
+	  cout << d[n][m];
+    return 0;
+}
